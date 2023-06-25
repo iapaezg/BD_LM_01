df984f199f3db2f3c9500f008469369dcfe6a27a
Ivantxo <iapaezg@unal.edu.co>
2023-06-25 15:20:26 -0500

    Update 3-Descriptiva.R

--- scripts/3-Descriptiva.R
+++ scripts/3-Descriptiva.R
@@ -29,7 +29,7 @@ sapply(libs, function(x) eval(req) || {install.packages(x); eval(req)})
 
 df <- import("https://github.com/iapaezg/BD_LM_01/raw/main/stores/data_final.rds")
 
-# Descriptivas ------------------------------------------------------------
+# Descriptivas PENDIENTE------------------------------------------------------------
 summary(df)
 skim(df)
 s_descrip <- skim(df)
@@ -54,102 +54,5 @@ boxplot(df$exp,ylab="Experiencia potencial (años)")
 histogram(df$estrato1,xlab="Estrato",ylab="Porcentaje",col="grey")
 histogram(df$maxEducLevel,xlab="Máximo nivel educativo",ylab="Porcentaje",col="grey")
 histogram(df$hijos,xlab="Número de hijos",ylab="Porcentaje",col="grey")
-pie(df$sex)
-
-hijo_sex <- ggplot(data=df) +
-  geom_histogram(mapping=aes(x=hijos,group=as.factor(sex),fill=as.factor(sex)))
-hijo_sex
-hijo_sex + scale_fill_manual(values=c("0"="red","1"="blue"),label=c("0"="Mujer","1"="Hombre"),name="Género")
-ggplot(data=df,mapping=aes(x=hijos,y=sex))
-ggplot(df,aes(x=hijos,color=sex)) +
-  geom_histogram(fill="white")
-
-boxplot(df$ln_income~df$sex,ylab="Ln ingreso/hora",xlab="Género",)
-
-
-stargazer(df)
-ls()
-summary(age_df$age)
-boxplot(age_df$age)
-names(df)
-
-ARTofR::xxx_title2('your title')
-ARTofR::xxx_title3('your sub-title')
-
-df <- df %>% rename(ID_base=df[1])
-#age_data<-df %>% filter(df$age>=18)
-sum(is.na(df$age))
-df %>% count(df$age)
-skim(df$age)
-
-
-###IVAN - - - - - - ## -> Ojo esto no lo corran estoy intentando unas cosas
-#age_data<-data_lim %>% filter(data_lim$age>=18)
-url_data <- RCurl::getURL("https://github.com/iapaezg/BD_LM_01/blob/d93cc92081ff0bd94f293e937f09b12676d6c29f/stores/df_raw.rds")
-df <- load(url(url_data))
-
-data_lim <- readRDS("df_raw.rds")
-summary(data_lim$age)
-age_data<-data_lim %>% subset(data_lim$age>=18)
-
-
-age_data
-summary(age_data$age)
-var <- c("directorio","secuencia_p", "orden", "clase", "estrato1", "sex", "age" %>% 
-           "p6050","oficio","age","clase","college","maxEducLevel","cotPension" %>% 
-           "dsi","formal","informal","pea","pet","wap","totalHoursWor_d","y_ingLab_m" %>% 
-           "y_ingLab_m_ha","y_otros_m","y_salary_m","y_salary_m_hu","y_total_m","y_total_m_ha")
-df <- age_data %>% filter(var,stars_with("iof"),stars_with("ingtot"))
-dsi
-estrato1
-fex_c
-fex_dpto
-formal
-informal
-oficio
-relab
-pea---poblacion economicamente activa
-pet---poblacion en edad de trabjar
-wap-- poblacion en edade de trabajar
-ocu
-iof----------todos
-ingtot ----- todos
-p6050
-directorio
-secuenciap
-orden
-clase
-dominio
-sex
-totalHoursWor_d
-y_ingLab_m
-y_ingLab_m_ha
-y_otros_m
-y_salary_m
-y_salary_m_hu
-y_total_m
-y_total_m_ha
-)
-df <- subset(age_data,select=)
-
-table(dataf$dominio)
-table(dataf$secuencia_p)
-boxplot(dataf$age)
-summary(dataf$age)
-dataf %>%
-  mutate(id_hogares=paste0(dataf$directorio,dataf$secuencia_p))
-dataf %>%
-  count(dataf$directorio,dataf$secuencia_p)
-dataf %>%
-  group_by(directorio,secuencia_p) %>%
-  summarize(count=n())
-dataf %>%
-  group_by(directorio) %>%
-  summarize(count=n())
-summary(dataf$age)
-summary(dataf$sex) # sex	=1 male, =0 female
-table(dataf$sex)
-age_data<-subset(dataf,dataf$age>=18)
-summary(age_data$age)
-str(age_data)
-ls(age_data)
+histogram(df$relab,xlab="Relación laboral",ylab="Porcentaje",col="grey")
+histogram(~hijos|sex,data=df,layout=c(2,1),col="gray",xlab="Número de hijos",ylab="Porcentaje")
