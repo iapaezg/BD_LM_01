5773115a7eee7a1953b58229c713ee2d93a10770
Ivantxo <iapaezg@unal.edu.co>
2023-06-25 19:15:37 -0500

    Update 4-2-Regresiones_sex.R

---
+++ scripts/4-2-Regresiones_sex.R
@@ -0,0 +1,25 @@
+# Cargue datos -----------------------------------------------------
+rm(list=ls())
+if(!require(pacman)) install.packages(pacman) ; require(pacman)
+require(pacman)
+
+if(!require(stargazer)) install.packages("stargazer") ; require(stargazer)
+require(stargazer)
+
+if(!require(sjmisc)) install.packages("sjmisc") ; require(sjmisc)
+require(sjmisc)
+
+if(!require(devtools)) install.packages("devtools") ; require(devtools)
+require(devtools)
+
+require(grDevices)
+
+p_load(rio, # import/export data
+       tidyverse, # tidy-data
+       skimr, # summary data
+       caret,
+       stargazer,
+       ggplot2)# Classification And REgression Training
+
+p_load(car,tidyverse,fixest, stargazer,knitr,kableExtra,jtools,ggstance,broom,broom.mixed,skimr)
+df <- import("https://github.com/iapaezg/BD_LM_01/raw/main/stores/data_final.rds")
