17f29141f652ff1e43381bf04716046681d8caad
hermes181192 <melissaninogutierrez@gmail.com>
2023-06-25 19:32:04 -0500

    Update 4-2-Regresiones_sex.R

--- scripts/4-2-Regresiones_sex.R
+++ scripts/4-2-Regresiones_sex.R
@@ -23,3 +23,63 @@ p_load(rio, # import/export data
 
 p_load(car,tidyverse,fixest, stargazer,knitr,kableExtra,jtools,ggstance,broom,broom.mixed,skimr)
 df <- import("https://github.com/iapaezg/BD_LM_01/raw/main/stores/data_final.rds")
+
+## gender-wage profile --------------------------------------------------------
+
+X <- df %>% 
+  select(sex)
+y <- df %>% 
+  select(ln_income) %>% 
+  rename(y=ln_income)
+dat=cbind(X,y)
+mod_sex <- lm(y~.,data=dat,x=TRUE)
+stargazer(mod_sex,type="text",dep.var.labels = c("Modelo ingreso hora con el género"),
+          digits = 4, out="mod_sex.txt")
+
+# Determina y gorro y residuos del modelo
+y_hat <- fitted(mod_sex)
+res <- resid(mod_sex)
+dat_p <- cbind(dat,res,y_hat)
+
+# Grafica el estimado de la función
+ggplot(dat_p) +
+  geom_point(aes(x=sex,y=y_hat)) +
+  ggtitle("Relación género-ingresos") +
+  labs(x="Género", y="Predicción ln ingresos/hora")
+
+# Analisis de residuos
+qqnorm(res)
+qqline(res)
+plot(res,dat$y,ylab = "Ln Ingreso/hora",xlab = "Residuos")
+residualPlots(mod_sex)
+
+
+# Bootstraping ------------------------------------------------------------
+p_load(boot)
+#boot(data,statistic,R)
+#Funcion
+set.seed(2023)
+beta_fn <- function(formula,data,indices){
+  d <- data[indices,]
+  fit <- lm(formula,data=d)
+  return(coef(fit))
+  }
+reps <- boot(data=dat,statistic=beta_fn,R=1000,formula=y~.)
+reps
+
+# Valor máximo
+set.seed(2023)
+R <- 1000
+reg_sex <- rep(0,R)
+for(i in 1:R) {
+  sample <- sample_frac(dat,size=1,repace=TRUE)
+  f <- lm(y~.,sample)
+  coefs <- f$coefficients
+  b1 <- coefs[2]
+  b2 <- coefs[3]
+  reg_sex[i] <- b1/(-2*b2)
+}
+histogram(reg_age)
+max(reg_sex)
+
+
